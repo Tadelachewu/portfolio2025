d6c98042a977a64baaaada4d73ae6b8115a5eabc
Tadelachewu <tade2024bdu@gmail.com>
2026-04-24 11:59:20 +0300

    buid error fixed

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  serverExternalPackages: ['handlebars', 'dotprompt', 'genkit', '@genkit-ai/core'],
   images: {
     remotePatterns: [
       {
